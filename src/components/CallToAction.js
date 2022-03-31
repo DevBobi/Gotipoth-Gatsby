ad613219d2efcb72c25ebc9530cbbe9fe02135d3
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2022-03-31 21:28:36 +0600

    initial commit

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -60,17 +60,21 @@ export const CallToAction = () => {
                                     <p className="font-medium text-gray-300">User friend cloud control panel with API</p>
                                 </li>
                             </ul>
-                            <div>
-                                <button type='button' className="mr-10 inline-block px-6 py-3 text-sm font-medium text-white bg-indigo-600">
-                                    Sign Up Now
-                                </button>
-                                <button type='button' className="inline-block px-4 py-3 text-sm font-medium text-white bg-inherit border" >
-                                    <span className='pr-2'>
-                                        <img className="inline-block h-5 w-5 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                                        <img className="inline-block h-5 w-5 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                                    </span>
-                                    <span>Schedule Meeting</span>
-                                </button>
+                            <div className='flex'>
+                                <div className='pr-12'>
+                                    <button type='button' className="inline-block border border-indigo-600 px-6 py-3 text-sm font-medium text-white bg-indigo-600">
+                                        Sign Up Now
+                                    </button>
+                                </div>
+                                <div>
+                                    <button type='button' className="inline-block px-4 py-3 text-sm font-medium text-white bg-inherit border" >
+                                        <span className='pr-2'>
+                                            <img className="inline-block h-5 w-5 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                                            <img className="inline-block h-5 w-5 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                                        </span>
+                                        <span>Schedule Meeting</span>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
