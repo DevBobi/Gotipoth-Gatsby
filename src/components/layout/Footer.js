59a677319d295e6f9ce4322ff03ededa0779e4dc
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2022-03-31 16:45:55 +0600

    footer fixed

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -9,35 +9,34 @@ const Footer = () => (
       <div class="justify-items-center grid grid-cols-1 gap-8 pt-6 border-t border-gray-100 sm:grid-cols-2 lg:grid-cols-3">
 
         <div className='px-10'>
-          <p class="font-medium">
+          <p class="text-gray-700">
             Highly scalable
           </p>
 
-          <nav class="flex flex-col mt-4 space-y-2 text-sm text-gray-500">
-            <p class="hover:opacity-75" href="">Scale from one to thousands of cloud instances in minutes</p>
+          <nav class="flex flex-col mt-4 space-y-2 text-sm">
+            <p class="font-medium" href="">Scale from one to thousands of cloud instances in minutes</p>
             <a href="">Button1</a>
           </nav>
         </div>
         <div className='px-10'>
-          <p class="font-medium">
+          <p class="text-gray-700">
             Better performance
           </p>
-          <nav class="flex flex-col mt-4 space-y-2 text-sm text-gray-500">
-            <p class="hover:opacity-75" href="">Experience faster IOPS Speed and ultra low latency network</p>
+          <nav class="flex flex-col mt-4 space-y-2 text-sm">
+            <p class="font-medium" href="">Experience faster IOPS Speed and ultra low latency network</p>
             <a href="">Button1</a>
           </nav>
         </div>
-        <div className='px-10'>
-          <p class="font-medium">
+        <div className='pr-10'>
+          <p class="text-gray-700">
             Save big
           </p>
 
-          <nav class="flex flex-col mt-4 space-y-2 text-sm text-gray-500">
-            <p class="hover:opacity-75" href="">Substantial savings compared to other public clouds</p>
+          <nav class="flex flex-col mt-4 space-y-2 text-sm">
+            <p class="font-medium" href="">Substantial savings compared to other public clouds</p>
             <a href="">Button1</a>
           </nav>
         </div>
-
       </div>
       <OurClients />
     </div>
