9063a35128aa48ac0e2a5fb52ffe0da9d86e4e7a
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2022-03-31 21:56:34 +0600

    final commiy

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -5,16 +5,16 @@ export const SideNav = () => {
         <div>
             <nav className=" lg:w-56 md:w-48 px-5 lg:pt-20 ">
                 <ul className='flex md:block md:px-5 sm:pr-3 xs:pr-3 justify-center'>
-                    <li className="mt-5 cursor-pointer text-sm xs:text-xs truncate text-left xs:pr-4">
+                    <li className="mt-5 cursor-pointer text-sm xs:text-xs truncate text-left xs:pr-3">
                         <span className="font-semibold text-gray-600 hover:text-gray-900">Featured</span>
                     </li>
-                    <li className=" mt-5 cursor-pointer text-sm xs:text-xs truncate text-left xs:pr-4">
+                    <li className=" mt-5 cursor-pointer text-sm xs:text-xs truncate text-left xs:pr-3">
                         <span className="font-semibold text-gray-600 hover:text-gray-900">Cloud Compute</span>
                     </li>
-                    <li className="mt-5 cursor-pointer text-sm xs:text-xs truncate text-left xs:pr-4">
+                    <li className="mt-5 cursor-pointer text-sm xs:text-xs truncate text-left xs:pr-3">
                         <span className="font-semibold text-gray-600 hover:text-gray-900">Network & CDN</span>
                     </li>
-                    <li className="mt-5 cursor-pointer text-sm xs:text-xs truncate text-left xs:pr-4">
+                    <li className="mt-5 cursor-pointer text-sm xs:text-xs truncate text-left xs:pr-3">
                         <span className="font-semibold text-gray-600 hover:text-gray-900">Cloud Storage</span>
                     </li>
                 </ul>
